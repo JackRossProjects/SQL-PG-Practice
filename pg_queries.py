e51c49971c26688ad49f55f6700fa7acd43859b7
User <jackrosscoding@gmail.com>
2020-03-17 15:07:19 -0600

    practice

---
+++ pg_queries.py
@@ -0,0 +1,22 @@
+# sql_practice/pg_queries.py
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv() # look in the .env file for env vars, and add them to the env
+
+DB_NAME = os.getenv("DB_NAME", default="OOPS")
+DB_USER = os.getenv("DB_USER", default="OOPS")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
+DB_HOST = os.getenv("DB_HOST", default="OOPS")
+
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+print("CONNECTION:", connection)
+
+cursor = connection.cursor()
+print("CURSOR:", cursor)
+
+cursor.execute("SELECT * from test_table;")
+result = cursor.fetchone()
+print("RESULT:", result)
